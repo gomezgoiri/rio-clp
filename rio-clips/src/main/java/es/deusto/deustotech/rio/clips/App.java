c293625de7f38b8e59c4ff55ed886ccde0d4a3bb
Aitor Gómez Goiri <aitor.gomez@deusto.es>
2013-09-06 11:54:31 +0200

    Basic Maven structure created

---
+++ rio-clips/src/main/java/es/deusto/deustotech/rio/clips/App.java
@@ -0,0 +1,13 @@
+package es.deusto.deustotech.rio.clips;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
